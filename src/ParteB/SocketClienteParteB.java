bcb0048de1afcdb0a36ec0df12c05a3556010b02
Samueh <samujedi@gmail.com>
2022-11-15 20:11:19 +0100

    casi terminado te enterah joseh

--- src/ParteB/SocketClienteParteB.java
+++ src/ParteB/SocketClienteParteB.java
@@ -26,14 +26,14 @@ public class SocketClienteParteB {
 
 			boolean continuar = true;
 			String opcion;
-			do { 
+			do {
 				Socket socketAlServidor = new Socket();
 				socketAlServidor.connect(direccionServidor);
 
 				PrintStream salida = new PrintStream(socketAlServidor.getOutputStream());
 
-				System.out.println(
-						"Elija la opcion que quiera realizar: \n 1-Sumar \n 2-Restar \n 3-Multiplicar \n 4-Dividir \n 5-Salir");
+				System.out
+						.println("Elija la opcion que quiera realizar: \n 1-Piedra \n 2-Papel \n 3-Tijera \n 4-Sair ");
 				System.out.print("Opcion:");
 
 				boolean valido = false;
@@ -41,31 +41,19 @@ public class SocketClienteParteB {
 				do {
 					opcion = sc.nextLine();
 
-					if (opcion.equals("1") || opcion.equals("2") || opcion.equals("3") || opcion.equals("4")
-							|| opcion.equals("5")) {
+					if (opcion.equals("1") || opcion.equals("2") || opcion.equals("3") || opcion.equals("4")) {
+						salida.println(opcion);
 						valido = true;
+					} else if (opcion.equals("4")) {
+						continuar = false;
+						salida.println(opcion);
+						imprimir = false;
+
 					} else {
 						System.out.println("Ingrese una opción válida");
 					}
 
 				} while (valido != true);
-				salida.println(opcion);
-
-				if (!opcion.equals("5")) {
-
-					System.out.println("Introduzca los numeros a operar:");
-					String numero1 = sc.nextLine();
-					String numero2 = sc.nextLine();
-					String operandos = numero1 + "-" + numero2;
-					salida.println(operandos);
-				} else if (opcion.equals("5")) {
-					continuar = false;
-					String numero1 = "0";
-					String numero2 = "0";
-					String operandos = numero1 + "-" + numero2;
-					imprimir = false;
-					salida.println(operandos);
-				}
 
 				// System.out.println("CLIENTE: Esperando a que el servidor acepte la
 				// conexi�n");
@@ -75,9 +63,15 @@ public class SocketClienteParteB {
 				// System.out.println("CLIENTE: Esperando al resultado del servidor...");
 				InputStreamReader entrada = new InputStreamReader(socketAlServidor.getInputStream());
 				BufferedReader bf = new BufferedReader(entrada);
-				String resultado = bf.readLine();
+				String puntos1 = bf.readLine();
+				String puntos2 = bf.readLine();
+
+				
 				if (imprimir) {
-					System.out.println("CLIENTE: El resultado de la operacion es: " + resultado);
+						
+					System.out.println("Marcador:");
+					System.out.println("Jugador 1: " + puntos1);
+					System.out.println("Jugador 2: " + puntos2 + "\n");
 
 				}
 
---
+++ src/ParteB/SocketServidorClientB.java
@@ -0,0 +1,126 @@
+package ParteB;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class SocketServidorClientB {
+
+	public static final int PUERTO = 2020;
+
+	public static void main(String[] args) throws InterruptedException {
+
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("      APLICACI�N DE SERVIDOR      ");
+		System.out.println("----------------------------------");
+
+		InputStreamReader entrada = null;
+
+		PrintStream salida = null;
+
+		Socket socketAlCliente = null;
+
+		InetSocketAddress direccion = new InetSocketAddress(PUERTO);
+
+		try (ServerSocket serverSocket = new ServerSocket()) {
+			serverSocket.bind(direccion);
+			String opcionRecibida;
+
+			int puntos1 = 0;
+			int puntos2 = 0;
+
+			boolean aux = true;
+
+			do {
+
+				System.out.println("SERVIDOR: Esperando peticion por el puerto " + PUERTO);
+
+				socketAlCliente = serverSocket.accept();
+
+				entrada = new InputStreamReader(socketAlCliente.getInputStream());
+				BufferedReader bf = new BufferedReader(entrada);
+
+				opcionRecibida = bf.readLine();
+
+				System.out.println("SERVIDOR: El jugador 1 eligio accion, te toca elegir");
+
+				System.out.println("-Jugador dos que eliges?");
+				int miOpcion = in.nextInt();
+
+				String j1 = opcionRecibida;
+				String j2 = String.valueOf(miOpcion);
+
+				if (j1.equals("1") && j2.equals("1")) {
+					System.out.println("Empate");
+
+				} else if (j1.equals("1") && j2.equals("2")) {
+					puntos2 = puntos2 + 1;
+					System.out.println("Gana la ronda el jugador 2\n");
+
+				} else if (j1.equals("1") && j2.equals("3")) {
+					puntos1++;
+					System.out.println("Gana la ronda el jugador 1\n");
+
+				} else if (j1.equals("2") && j2.equals("2")) {
+					System.out.println("Empate");
+
+				} else if (j1.equals("2") && j2.equals("1")) {
+					puntos1++;
+					System.out.println("Gana la ronda el jugador 1\n");
+
+				} else if (j1.equals("2") && j2.equals("3")) {
+					puntos2++;
+					System.out.println("Gana la ronda el jugador 2\n");
+
+				} else if (j1.equals("3") && j2.equals("3")) {
+					System.out.println("Empate");
+
+				} else if (j1.equals("3") && j2.equals("1")) {
+					puntos1++;
+					System.out.println("Gana la ronda el jugador 1\n");
+
+				} else if (j1.equals("3") && j2.equals("2")) {
+					puntos2++;
+					System.out.println("Gana la ronda el jugador 2\n");
+
+				} else {
+					System.out.println("Datos introducidos NO VALIDOS");
+				}
+
+				salida = new PrintStream(socketAlCliente.getOutputStream());
+
+				System.out.println("Marcador:");
+				System.out.println("Jugador 1: " + puntos1);
+				System.out.println("Jugador 2: " + puntos2 + "\n");
+
+				salida.println(puntos1);
+				salida.println(puntos2);
+
+				if (puntos1 >= 3) {
+					System.out.println("------El jugador 1 ha ganado la partida a tres rondas------");
+					aux = false;
+					socketAlCliente.close();
+				}
+				if (puntos2 >= 3) {
+					System.out.println("------El jugador 2 ha ganado la partida a tres rondas------");
+					aux = false;
+					socketAlCliente.close();
+
+				}
+			} while (aux);
+
+		} catch (IOException e) {
+			System.err.println("SERVIDOR: Error de entrada/salida");
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.err.println("SERVIDOR: Error -> " + e);
+			e.printStackTrace();
+		}
+	}
+}
